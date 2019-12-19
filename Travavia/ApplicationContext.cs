507dcacc988034efd34762f1a076f2e89278a0de
Марина Генкина <marinagenkina@gmail.com>
2019-12-20 00:20:34 +0300

    Connected a database to load flights.

---
+++ Travavia/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travavia.Model;
+
+namespace Travavia
+{
+    class ApplicationContext : DbContext 
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=flightsdb;Trusted_Connection=True;");
+        }
+    }
+}
--- Travavia/Program.cs
+++ Travavia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Travavia.Model;
 
 namespace Travavia
 {
@@ -10,6 +11,16 @@ namespace Travavia
     {
         static void Main(string[] args)
         {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Flights.ToList();
+                Console.WriteLine("Проверка данных:"); //Проверяем что база данных подгружается
+                foreach (Flight f in users)
+                {
+                    Console.WriteLine($"{f.ArrAirport}");
+                    Console.ReadKey(true);
+                }
+            }
         }
     }
 }
