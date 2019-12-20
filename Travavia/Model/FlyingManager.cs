5953dda0b4c1a7199a69e94f7fac81441e3b85ec
Марина Генкина <marinagenkina@gmail.com>
2019-12-20 21:33:44 +0300

    Change

--- Travavia/Model/FlyingManager.cs
+++ Travavia/Model/FlyingManager.cs
@@ -8,20 +8,17 @@ namespace Travavia.Model
 {
     public class FlyingManager
     {
-        //using (ApplicationContext db = new ApplicationContext())
-        //{
-        //    FlightInfo flightinfo1 = new FlightInfo {FlightInfoID = 1, NumberOfTickets = 5, Price = 3000 };
-        //    db.FlightInfo.Add(flightinfo1);
-        //    db.SaveChanges();
-        //}
+        
         public List<Flight> Flights { get; set; }
         public List<FlightInfo> FlightInfo { get; set; }
+
         public FlyingManager()
         {
             Flights = GetFlights();
             FlightInfo = GetDetails();
         }
-        protected List<Flight> GetFlights() 
+
+        public List<Flight> GetFlights() 
         {
             using (ApplicationContext db = new ApplicationContext())
             {
@@ -30,7 +27,7 @@ namespace Travavia.Model
                 return flights;
             }
         }
-        protected List<FlightInfo> GetDetails()
+        public List<FlightInfo> GetDetails()
         {
             using (ApplicationContext db = new ApplicationContext())
             {
